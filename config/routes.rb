986e8ccfb631c0d03b2d9185afd04c20b56b08d4
Jonathan Chan <johnkchan252@gmail.com>
2019-02-22 05:56:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists
-  
-  
-  resources :artists, only: [:show] do
-    # nested resource for posts
+
+  resources :artists,  do
     resources :songs, only: [:show, :index]
   end
   
